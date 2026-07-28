db4472e41dc95819ad98ee4ab2d1d205d07ec7ff
shivaamit950-coder <shivaamit950@gmail.com>
2026-07-28 10:23:18 +0530

    Update ChatRoom.jsx

--- ChatRoom.jsx
+++ ChatRoom.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
 import Link from "next/link";
-import { createClient } from "@/lib/supabaseClient";
+import { createClient } from "@/supabaseClient";
 
 export default function ChatRoom({ conversationId, currentUserId, otherProfile, initialMessages }) {
   const [messages, setMessages] = useState(initialMessages);
