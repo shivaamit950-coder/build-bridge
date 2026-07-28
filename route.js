5b94772a60df1bdf8fa3bde939afdedb2105d2e5
shivaamit950-coder <shivaamit950@gmail.com>
2026-07-28 15:49:35 +0530

    Update route.js

--- route.js
+++ route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { createClient } from "@/supabaseClient";
+import { PROVIDERS } from "@/aiProviders";
 
 export async function GET() {
   return NextResponse.json({ providers: PROVIDERS });
